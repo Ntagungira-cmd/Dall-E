bdf1db11275e87c644d7819ea4d9c3d44d55cad0
ntagungira-cmd <ntagungiraali@gmail.com>
2023-03-08 14:47:43 +0200

    fix border-b warning

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <header className="w-full flex justify-between items-center bg-white
-        sm:px-8 px-4 py-4 border-b border-b
+        sm:px-8 px-4 py-4 border-b
         [#e6ebf4]">
         <Link to="/">
           <img src={logo} alt="logo" className="w-28 object-contain" />
