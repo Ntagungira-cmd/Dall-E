7cae218abb8f03a8c411d50c23ff05d0dbeb9d24
Ntagungira Ali Rashid <83630076+Ntagungira-cmd@users.noreply.github.com>
2023-09-25 06:35:36 +0200

    Update CreatePost.jsx

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -29,7 +29,7 @@ const CreatePost = () => {
     if (form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch('https://dall-e-production-ali.up.railway.app/api/v1/dalle', {
+        const response = await fetch('https://dalle-api-fqj3.onrender.com/api/v1/dalle', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -58,7 +58,7 @@ const CreatePost = () => {
     if (form.prompt && form.photo) {
       setLoading(true);
       try {
-        const response = await fetch('https://dall-e-production-ali.up.railway.app/api/v1/post', {
+        const response = await fetch('https://dalle-api-fqj3.onrender.com/api/v1/post', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
