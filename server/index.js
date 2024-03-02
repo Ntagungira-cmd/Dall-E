90289535d1a0860c86fe6114f3696d31e823fb05
Ntagungira Ali Rashid <83630076+Ntagungira-cmd@users.noreply.github.com>
2024-03-02 21:40:57 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors());
-app.use(express.json({ limit: '50mb' }));
+app.use(express.json({ limit: '100mb' }));
 app.use('/api/v1/post', postRoutes);
 app.use('/api/v1/dalle', dalleRoutes);
 
